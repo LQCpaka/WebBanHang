4c2619711c888b15b0d490e952a575df461da434
LQCpaka <boyrovip1539@gmail.com>
2024-06-04 10:59:37 +0700

    edit commit

--- WebBanHang/Models/Category.cs
+++ WebBanHang/Models/Category.cs
@@ -5,9 +5,9 @@ namespace WebBanHang.Models
     public class Category
     {
         public int Id { get; set; }
-        [Required, StringLength(50)]
+        [Required(ErrorMessage = "Chưa Nhập Tên Chủng Loại"), StringLength(50)]
         public String Name { get; set; }
-        [Range(1, 100)]
+        [Range(1, 100, ErrorMessage = "Trong khoảng 1 - 100")]
         public int DisplayOrder { get; set; }
     }
 }
